01eff78092247cf0aea0307beee2bbb19f3259b9
Adriana Allegra <adriana.allegrabelvedere@gmail.com>
2021-10-13 17:16:18 -0300

    carrito

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ const producto4 = new Producto('Iluminador', 600, 50, "img/producto4.jpg", false
 const producto5 = new Producto('Rimel', 950, 70, "img/producto5.jpg", false);
 const producto6 = new Producto('Agua Micelar', 1300, 40, "img/producto6.jpg", false);
 const producto7 = new Producto('Serum Reparador', 750, 48, "img/producto7.jpg", false);
-const producto8 = new Producto('Roller de jade', 600, 50, "img/producto8.jpg", true);
+const producto8 = new Producto('Roller de jade', 600, 50, "img/producto8.jpg", true); 
 const producto9 = new Producto('Crema hidratante de manos', 800, 60, "img/producto9.jpg", false);
 const producto10 = new Producto('Base de maquillaje', 1400, 35, "img/producto10.jpg", false);
 const producto11 = new Producto('Kit 4 labiales', 1650, 20, "img/producto11.jpg", true);
@@ -18,6 +18,7 @@ let carrito =[];
 let acumulador = ``;
 let acumuladorDescuentos = ``;
 let productosCarrito = '';
+
 obtenerProductosPublicitarios();
 
 productos.forEach((producto) => {
@@ -25,14 +26,14 @@ productos.forEach((producto) => {
     acumuladorDescuentos += `<div class="col mb-5">
     <div class="card h-100">
         <img class="card-img-top" src="${producto.img}" alt="..." />                            
-        <div class="card-body p-4">
-            <div class="text-center">
+        <div class="card-body p-4" >
+            <div class="text-center" id="info-card" >
                 <h5 class="fw-bolder"> ${producto.nombre} </h5>
                 <p> $ ${producto.precio} </p>
             </div>
         </div>
         <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
-            <div class="text-center"> <button class="btn btn-outline-dark mt-auto" href="#" onclick="agregarAlCarrito('${producto.nombre}')">Agregar al carrito</button>
+            <div class="text-center"> <button class="btn btn-outline-dark mt-auto"  href="#" onclick="agregarAlCarrito('${producto.nombre}')">Agregar al carrito</button>
             </div>
         </div>
 
@@ -43,7 +44,7 @@ productos.forEach((producto) => {
     <div class="card h-100">
         <img class="card-img-top" src="${producto.img}" alt="..." />                            
         <div class="card-body p-4">
-            <div class="text-center">
+            <div class="text-center" id="info-card">
                 <h5 class="fw-bolder"> ${producto.nombre} </h5>
                 <p> $ ${producto.precio} </p>
             </div>
@@ -56,6 +57,7 @@ productos.forEach((producto) => {
     </div>
 </div>`;
    }
+   
 })
 
 $('#productos').append(acumulador)
@@ -63,11 +65,28 @@ $('#descuentos').append(acumuladorDescuentos);
 
 
 
-function agregarAlCarrito(title){
-    const productoEncontrado = productos.find(productos => productos.nombre === title )
+function agregarAlCarrito(titulo){
+    const productoEncontrado = productos.find(productos => productos.nombre === titulo )
+    let acumuladorCarrito = ``;
+    let precioTotal = 0;
+ 
+
     carrito.push(productoEncontrado);
 
+    carrito.forEach(producto => {
+        acumuladorCarrito += ` <tr>
+            <td>${producto.nombre}</td>
+            <td>${producto.precio}</td>
+        </tr> `;
+        precioTotal += producto.precio;
+        });
+    
+
     document.getElementById('contador-carrito').innerHTML = carrito.length
+    document.getElementById('tbody').innerHTML = acumuladorCarrito;
+    document.getElementById('precio-total').innerHTML = `Total: ${precioTotal}`;
+
+
    
 }
 
